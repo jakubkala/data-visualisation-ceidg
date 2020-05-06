566758c7e9e9314c19bd92061b7bf3379f6b24b9
Michal Bortkiewicz <michalbortkiewicz8@gmail.com>
2020-05-06 22:04:57 +0200

    lil refactor

--- models/dalex_tests.R
+++ models/dalex_tests.R
@@ -3,44 +3,18 @@ library("DALEX")
 library("randomForest")
 library("dplyr")
 
-model <- randomForest(status ~ . , data = HR)
-
-#
-# Create a DALEX explainer
-explainer_rf_fired <- explain(model,
-                              data = HR,  y = HR$status == "fired",
-                              predict_function = function(m,x) predict(m,x, type = "prob")[,1])
-
-#
-# Calculate variable attributions
-new_observation <- HRTest[12,]
-library("breakDown")
-bd_rf <- break_down(explainer_rf_fired,
-                    new_observation,
-                    keep_distributions = TRUE)
-
-bd_rf
-
-
-plot(bd_rf)
-
-
-plot(bd_rf, plot_distributions = TRUE)
-
-
 
 #####################################
-
 ceidg <- read.csv("data/dataset_cleaned_csv")
-# ceidg <- ceidg[c(1:50000), ]
-ceidg <- sample_n(ceidg, 10000)
-
-ceidg <- na.omit(ceidg)
+ceidg <- sample_n(ceidg, 10000) # this dataset is too large...
+ceidg <- na.omit(ceidg) # drop nans for rf
 
 # Too many levels of factors in below columns - need to test other models
 drops <- c("MainAddressCounty","CorrespondenceAddressCounty")
 ceidg <- ceidg[ , !(names(ceidg) %in% drops)]
 
+
+# Define model
 model <- randomForest(Target ~ . , data = ceidg)
 
 # Create a DALEX explainer
@@ -48,7 +22,7 @@ explainer_rf_fired <- explain(model,
                               data = ceidg,  y = ceidg$Target == "True",
                               predict_function = function(m,x) predict(m,x, type = "prob")[,1])
 
-#
+
 # Calculate variable attributions
 new_observation <- ceidg[12,]
 library("breakDown")
@@ -58,5 +32,9 @@ bd_rf <- break_down(explainer_rf_fired,
 
 bd_rf
 
-
+# Plot inference explaination 
 plot(bd_rf)
+
+# Plot wild graph (not sure what it is)
+plot(bd_rf, plot_distributions = TRUE)
+
